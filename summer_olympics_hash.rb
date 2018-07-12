9439f56f3c1f4e3a575415da3ddde1948593513f
alwaysmaria1 <alwaysmaria1@gmail.com>
2018-07-12 14:28:55 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -18,7 +18,7 @@ end
 
 def add_a_key_value_pair
   olympic_hash = create_olympics_hash
-  create_olympics_hash.store(:Atlanta, "1996")
+  olympic_hash.store(:Atlanta, "1996")
   return olympic_hash
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
 end
