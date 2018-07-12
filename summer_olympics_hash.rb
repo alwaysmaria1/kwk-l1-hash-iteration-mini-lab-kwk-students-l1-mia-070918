5ba42e58a5a6fbac60cb811fef15a4c89c353611
alwaysmaria1 <alwaysmaria1@gmail.com>
2018-07-12 14:26:14 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -16,13 +16,12 @@ end
   
   # Implement this method so that it returns a hash with the data provided on README.md
 
-def add_a_key_value_pair
-  
+def add_a_key_value_pair(create_olympics_hash)
   create_olympics_hash[:Atlanta]="1996"
   # return create_olympics_hash
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
 end
-return add_a_key_value_pair
+return add_a_key_value_pair(create_olympics_hash)
 
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
